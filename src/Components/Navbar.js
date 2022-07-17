2e422344e292d0fd75862b5d9a826a00609a9c26
venkat <srivenkatsairaj13@gmail.com>
2022-07-17 15:29:41 +0530

    add Components

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../react-icon-small.png";
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={logo} className="nav--icon logo" alt="react-logo" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
+}
